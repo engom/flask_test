c312f35cd3b7eb28551fe4f9a04acdca8416f339
Elhadji NGOM <elhadji.ngom@aims-senegal.org>
2022-04-15 10:55:18 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World, this flask script works too!</p>"
+    return "<p> Hello, World! This flask script works too. Thank you God. </p>"
